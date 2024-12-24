8e56fb6a6779164cd606ef63ee7d3a632651cdbf
giykim <giy.kim27@gmail.com>
2024-12-23 22:34:20 -0500

    Navbar and footer

--- portfolio/src/pages/Layout.js
+++ portfolio/src/pages/Layout.js
@@ -3,16 +3,23 @@ import "./Layout.css";
 
 const Layout = () => {
     return (
-        <div className="content">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/experience">Experience</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
+        <>
+            <div className="navbar">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/experience">Experience</Link></li>
+                    <li><Link to="/projects">Projects</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </div>
+
             <Outlet />
-        </div>
+
+            <footer className="footer">
+                <p>© {new Date().getFullYear()} Giyoung Kim. All rights reserved.</p>
+            </footer>
+        </>
     )
 };
 
