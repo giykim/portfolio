4ede9247508a75f59eb58effba439d9179529a17
giykim <giy.kim27@gmail.com>
2024-12-24 16:27:40 -0500

    Base urlas /portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -39,7 +39,7 @@ function Layout() {
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/portfolio">
             <Layout />
         </BrowserRouter>
     );
