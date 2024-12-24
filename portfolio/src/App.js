3d877fd82220d0cd3b2775b033787868ee8c9e4e
giykim <giy.kim27@gmail.com>
2024-12-23 21:17:53 -0500

    App routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,46 +1,94 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as BrowserRouter, Outlet, Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
+const Layout = () => {
+  return (
+    <div className="content">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/experience">Experience</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  )
+};
+
 const Home = () => (
-  <div className="Content">
-    Main
+  <div className="content">
+    <h2>
+      Home
+    </h2>
+  </div>
+);
+
+const About = () => (
+  <div className="content">
+    <h2>
+      About
+    </h2>
+  </div>
+);
+
+const Experience = () => (
+  <div className="content">
+    <h2>
+      Experience
+    </h2>
+  </div>
+);
+
+const Projects = () => (
+  <div className="content">
+    <h2>
+      Projects
+    </h2>
+  </div>
+);
+
+const Contact = () => (
+  <div className="content">
+    <h2>
+      Contact
+    </h2>
+  </div>
+);
+
+const NoPage = () => (
+  <div className="content">
+    <h2>
+      No Page Found
+    </h2>
   </div>
 );
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">
-                  Home
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <main>
-          <Routes>
-            <Route index="/" element={<Home />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="experience" element={<Experience />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
