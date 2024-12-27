cfdb226794f61e798d1e7498c8fa258b1b9369df
giykim <giy.kim27@gmail.com>
2024-12-27 14:14:45 -0500

    Update NoPage found page

--- portfolio/src/pages/NoPage.js
+++ portfolio/src/pages/NoPage.js
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
+import ScrollableSections from "../components/ScrollableSections";
 import "./NoPage.css";
+import Ease from "../components/Ease";
 
-const NoPage = () => (
-    <div class="content">
-        <h2>No Page Found</h2>
-    </div>
-);
+const NoPage = () => {
+    const sections = [
+        <>
+            <h1>No Page Found</h1>
+            <Ease>
+                <p><Link to="/">Return to Home</Link></p>
+            </Ease>
+        </>,
+    ];
+
+    return (
+        <div class="content">
+            <ScrollableSections sections={sections} />
+        </div>
+    )
+};
 
 export default NoPage
