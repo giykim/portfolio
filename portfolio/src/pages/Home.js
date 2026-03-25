da93caf854f73d1fd5bf1f094f784a60aa51882b
Giyoung Kim <gk122@duke.edu>
2026-03-25 00:49:55 -0500

    Add arrow indicator to home page links

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -8,13 +8,13 @@ const Home = () => {
             <div><code className="h1">Welcome to My Portfolio</code></div>
         </>,
         <>
-            <Link to="/health-assistant"><div><code className="h1">Health Assistant</code></div></Link>
+            <Link to="/health-assistant"><div><code className="h1">Health Assistant →</code></div></Link>
         </>,
         <>
-            <Link to="/steam-scout"><div><code className="h1">Steam Scout</code></div></Link>
+            <Link to="/steam-scout"><div><code className="h1">Steam Scout →</code></div></Link>
         </>,
         <>
-            <Link to="/about"><div><code className="h1">About</code></div></Link>
+            <Link to="/about"><div><code className="h1">About →</code></div></Link>
         </>,
     ];
 
