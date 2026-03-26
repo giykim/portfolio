dc05df2a97c685f6fd9bb6bb53b50c947de4b8dc
Giyoung Kim <gk122@duke.edu>
2026-03-25 19:20:01 -0500

    Push footer above search bar

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -13,7 +13,7 @@ const About = () => {
                     I'm a Software Development Engineer at <strong>AWS</strong> with dual B.S. degrees in Computer Science and Statistical Science from <strong>Duke University</strong>, passionate about using ML and AI to build better customer experiences.
                 </code>
                 <ul>
-                    <li><code className="p">Personal: building ML / AI projects, Counter-Strike, weightlifting, bouldering</code></li>
+                    <li><code className="p">Hobbies: building ML / AI projects, Counter-Strike, weightlifting, bouldering</code></li>
                     <li><code className="p">Shows: The Simpsons, Bojack Horseman, Breaking Bad</code></li>
                     <li><code className="p">Interests: Watches, FC Barcelona</code></li>
                 </ul>
